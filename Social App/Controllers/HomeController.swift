e884201bb106737c4b2e5d2457cee1e0be2e0320
MinThetMaung <minthetmaung96@gmail.com>
2020-09-09 14:39:39 +0630

    setup views for Login Controller

--- Social App/Controllers/HomeController.swift
+++ Social App/Controllers/HomeController.swift
@@ -9,16 +9,43 @@
 import UIKit
 import LBTATools
 
-class HomeController: UIViewController {
+class HomeController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        navigationItem.rightBarButtonItem = .init(title: "Fetch posts", style: .done, target: self, action: #selector(fetchPosts))
+        
+        navigationItem.leftBarButtonItem = .init(title: "Login", style: .plain, target: self, action: #selector(handleLogin))
+        navigationItem.rightBarButtonItem = .init(title: "Fetch posts", style: .plain, target: self, action: #selector(fetchPosts))
+    }
+    
+    @objc private func handleLogin() {
+        let navController = UINavigationController(rootViewController: LoginController())
+        present(navController, animated: true)
     }
     
     @objc private func fetchPosts() {
-        print("Attempting to fetch posts")
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjU3OTUyNmUwODIxYTIxZmIxMDYxOWUiLCJlbWFpbCI6ImRhbmllbEBnbWFpbC5jb20iLCJpYXQiOjE1OTk2MzA0MzEsImV4cCI6MTU5OTgwMzIzMX0.U_TIwUTHX4Zcgx0ym8SEdk5LMLvas2V-qw3A8FV-xXM"
+        guard let url = URL(string: "https://socialappnode.herokuapp.com/posts/") else { return }
+        var request = URLRequest(url: url)
+        request.setValue("\(token)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        URLSession.shared.dataTask(with: request) { (data, resp, err) in
+            
+            DispatchQueue.main.async {
+                if let err = err {
+                    print("Error in getting posts : \(err)")
+                    return
+                } else if let resp = resp as? HTTPURLResponse, resp.statusCode != 200 {
+                    print("Failed to fetch with status code \(resp.statusCode)")
+                } else {
+                    print("Sucessfully fetched posts")
+                    print(data)
+                }
+                
+            }
+            
+        }.resume()
     }
     
 }
---
+++ Social App/Controllers/LoginController.swift
@@ -0,0 +1,71 @@
+//
+//  LoginController.swift
+//  Social App
+//
+//  Created by Min Thet Maung on 09/09/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import LBTATools
+import Alamofire
+import JGProgressHUD
+
+class LoginController: LBTAFormController {
+    
+    // MARK: - UI Elements
+    let logoImageView = UIImageView(image: UIImage(named: "startup"), contentMode: .scaleAspectFit)
+    let logoLabel = UILabel(text: "FullStack Social", font: .systemFont(ofSize: 32, weight: .heavy), textColor: .black, numberOfLines: 0)
+    
+    let emailTextField = IndentedTextField(placeholder: "Email", padding: 24, cornerRadius: 25, keyboardType: .emailAddress)
+    let passwordTextField = IndentedTextField(placeholder: "Password", padding: 24, cornerRadius: 25)
+    lazy var loginButton = UIButton(title: "Login", titleColor: .white, backgroundColor: .black, target: self, action: #selector(handleLogin))
+    let errorLabel = UILabel(text: "Your login crenditals were incorrect, please try again.", font: .boldSystemFont(ofSize: 18), textColor: .red, textAlignment: .center, numberOfLines: 0)
+    lazy var goToRegisterButton = UIButton(title: "Don't have account? Go to register", titleColor: .black, font: .systemFont(ofSize: 16), target: self, action: #selector(goToRegister))
+    
+    
+    @objc private func handleLogin() {
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Logging in"
+        hud.show(in: view)
+        
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        errorLabel.isHidden = true
+    }
+    
+    @objc private func goToRegister() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .init(white: 0.95, alpha: 1)
+        
+        emailTextField.autocapitalizationType = .none
+        emailTextField.backgroundColor = .white
+        passwordTextField.backgroundColor = .white
+        passwordTextField.isSecureTextEntry = true
+        loginButton.layer.cornerRadius = 25
+        navigationController?.navigationBar.isHidden = true
+        errorLabel.isHidden = true
+        
+        let formView = UIView()
+        formView.stack(
+            formView.stack(formView.hstack(logoImageView.withSize(.init(width: 80, height: 80)), logoLabel.withWidth(160), spacing: 16, alignment: .center).padLeft(12).padRight(12), alignment: .center),
+            UIView().withHeight(12),
+            emailTextField.withHeight(50),
+            passwordTextField.withHeight(50),
+            loginButton.withHeight(50),
+            errorLabel,
+            goToRegisterButton,
+            UIView().withHeight(80),
+            spacing: 16).withMargins(.init(top: 48, left: 32, bottom: 0, right: 32))
+        
+        formContainerStackView.padBottom(-24)
+        formContainerStackView.addArrangedSubview(formView)
+    }
+    
+
+}
